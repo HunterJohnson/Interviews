31711a96732301d119910b5f3c94f1aa3e75c84d
Hunter Johnson <hhjohnso@indiana.edu>
2018-09-04 19:10:11 -0400

    Update bitwise.py

--- epip/bitwise.py
+++ epip/bitwise.py
@@ -137,18 +137,7 @@ def power(x, y): # O(n) power, # of multiplications is at most 2x the index of y
     return -result if x < 0 else result
   
   
-def is_palindrome_number(x):
-  if x <= 0:
-    return x == 0
-  num_digits = math.floor(math.log10(x)) + 1
-  msd_mask = 10**(num_digits - 1)
-  for i in range(num_digits // 2):
-    if x // msd_mask != x % 10:
-      return False
-    x  %= msd_mask
-    x //= 10
-    msd_mask //= 100
-  return True
+
 
 
   
