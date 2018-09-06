af3a320689f462f498d54a51c7d46e413e328416
Hunter Johnson <hhjohnso@indiana.edu>
2018-09-06 15:27:09 -0400

    Update linked_list.py

--- epip/linked_list.py
+++ epip/linked_list.py
@@ -95,6 +95,33 @@ class LinkedList:
         while (temp):
             print temp.data,
             temp = temp.next
+         
+    def reverseUtil(self, curr, prev):
+         
+        # If last node mark it head
+        if curr.next is None :
+            self.head = curr 
+             
+            # Update next to prev node
+            curr.next = prev
+            return
+         
+        # Save curr.next node for recursive call
+        next = curr.next
+ 
+        # And update next 
+        curr.next = prev
+     
+        self.reverseUtil(next, curr) 
+ 
+ 
+    # This recursive reverse function mainly calls reverseUtil()
+    # with previous as None
+    def reverse_rec(self):
+        if self.head is None:
+            return
+        self.reverseUtil(self.head, None)
+ 
  
  
  
